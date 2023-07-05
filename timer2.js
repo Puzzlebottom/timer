f030e3f6135269cc2c3edd69e4d2fc4ef5f0beb9
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-04 22:17:53 -0400

    completes function

---
+++ timer2.js
@@ -0,0 +1,37 @@
+
+process.stdin.setRawMode(true);
+process.stdin.setEncoding('utf8');
+
+
+process.stdin.on('data', (key) => {
+
+  const ctrlC = '\x03';
+  const NUMBERS = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+  const beep = () => process.stdout.write("\x07");
+
+  const beepAfter = (seconds) => {
+
+    const convertToMilliseconds = (seconds) => seconds * 1000;
+    const delay = convertToMilliseconds(seconds);
+
+    setTimeout(() => {
+      // process.stdout.write("\x07");
+      process.stdout.write("ALARM!");
+    }, delay);
+  };
+
+  if (key === 'b') {
+    beep();
+  }
+
+  if (NUMBERS.includes(key)) {
+    process.stdout.write(`setting timer for ${key} seconds...\n`);
+    beepAfter(Number(key));
+  }
+
+  if (key === ctrlC) {
+    process.stdout.write('Thanks for using me, ciao!\n');
+    process.exit();
+  }
+});
